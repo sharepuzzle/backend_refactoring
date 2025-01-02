730860cd03e38842073e0a0b5639f52b62040b32
jifrozen0110 <answldjs1836@gmail.com>
2025-01-02 15:43:49 +0900

    chore : 프로젝트 세팅

---
+++ src/main/java/com/puzzle/ps/PsRefactorApplication.java
@@ -0,0 +1,13 @@
+package com.puzzle.ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsRefactorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PsRefactorApplication.class, args);
+  }
+
+}
